404be585b641d19c9a080f3b268fbb76ba0c7ce9
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-16 14:39:27 -0400

    Successfully created and ran sanity check on both thoughter functions

--- test/specs/recent-thoughts.spec.js
+++ test/specs/recent-thoughts.spec.js
@@ -6,9 +6,17 @@
   describe('recent-thoughts module', function() {
     describe('showRecent function', function() {
 
+      it('should be a function', function() {
+        expect( window.thoughter.showRecent ).to.be.a.function;
+      });
+
     });
 
     describe('getRecent function', function() {
+
+      it('should be a function', function() {
+        expect( window.thoughter.getRecent ).to.be.a.function;
+      });
 
     });
   });
