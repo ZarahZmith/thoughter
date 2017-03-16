0088bae7f388e7723da9cc80a63958c01aa282b4
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-16 15:38:49 -0400

    Created and sucessfully passed test to see if function ran

--- test/specs/recent-thoughts.spec.js
+++ test/specs/recent-thoughts.spec.js
@@ -22,32 +22,41 @@
         expect( window.thoughter.showRecent ).to.be.a.function;
       });
 
-    });
+      it('should run the function', function() {
+        let result = window.thoughter.showRecent([
+          {content:'THIS IS HARD', createTime: 'lkjhgfd', id: '7397258738'}
+        ]);
+        let articles = document.querySelectorAll('main article');
 
-    describe('getRecent function', function() {
-
-      beforeEach(function() {
-        server = sinon.fakeServer.create();
-        server.autoRespond = true;
-        server.respondWith(
-          'GET',
-          'http://thoughter.herokuapp.com/api/Thoughts?filter={"order":"createTime DESC","limit":30}',
-          [
-            200,
-            { 'Content-Type': 'appliaction/json'},
-            '[ { "name":"Jordan" }, { "name":"Julianne" } ]'
-          ]
-        );
-      });
-
-      afterEach(function() {
-        server.restore();
-      });
-
-      it('should be a function', function() {
-        expect( window.thoughter.getRecent ).to.be.a.function;
+        expect(articles.length).to.equal(1);
       });
 
     });
+
+    // describe('getRecent function', function() {
+    //
+    //   beforeEach(function() {
+    //     server = sinon.fakeServer.create();
+    //     server.autoRespond = true;
+    //     server.respondWith(
+    //       'GET',
+    //       'http://thoughter.herokuapp.com/api/Thoughts?filter={"order":"createTime DESC","limit":30}',
+    //       [
+    //         200,
+    //         { 'Content-Type': 'appliaction/json'},
+    //         '[ { "name":"Jordan" }, { "name":"Julianne" } ]'
+    //       ]
+    //     );
+    //   });
+    //
+    //   afterEach(function() {
+    //     server.restore();
+    //   });
+    //
+    //   it('should be a function', function() {
+    //     expect( window.thoughter.getRecent ).to.be.a.function;
+    //   });
+    //
+    // });
   });
 }());
