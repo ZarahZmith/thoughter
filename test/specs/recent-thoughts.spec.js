25aeff22eddd47190435f992bfe48a907f255ba4
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-16 14:30:13 -0400

    Did initial set up for files assoiciated with tests including the set up for sinon; process described in README.md

---
+++ test/specs/recent-thoughts.spec.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  let expect = chai.expect;
+
+  describe('recent-thoughts module', function() {
+    describe('showRecent function', function() {
+
+    });
+
+    describe('getRecent function', function() {
+
+    });
+  });
+}());
